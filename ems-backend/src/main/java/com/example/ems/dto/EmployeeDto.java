5bb7cd9e72ecba39054e0fb58ed31e480d24b9f6
narendra-003 <narendranandaniya43@gmail.com>
2025-01-22 09:23:38 +0530

    post and get employee rest endpont

--- ems-backend/src/main/java/com/example/ems/dto/EmployeeDto.java
+++ ems-backend/src/main/java/com/example/ems/dto/EmployeeDto.java
@@ -1,10 +1,14 @@
 package com.example.ems.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDto {
     private Long id;
     private String firstName;
--- ems-backend/src/main/java/com/example/ems/entity/Employee.java
+++ ems-backend/src/main/java/com/example/ems/entity/Employee.java
@@ -1,11 +1,15 @@
 package com.example.ems.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "employees")
 public class Employee {
